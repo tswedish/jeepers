58fc244ef2045256b1abb14842c1a624644cd4af
tswedish <tristan@voldemort.media.mit.edu>
2017-03-21 14:01:06 -0400

    add third party version controlled download/install

---
+++ install_thirdparty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir util_thirdparty
+cd util_thirdparty
+wget https://raw.githubusercontent.com/tswedish/fb.resnet.torch/jeepers-transforms/datasets/transforms.lua
+wget https://raw.githubusercontent.com/tswedish/trainplot/jeepers-logging/trainplot.lua
