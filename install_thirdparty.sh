7a2406e75bc4fbc8efdd07bc0226363293c349b1
tswedish <tristan@voldemort.media.mit.edu>
2017-03-21 14:22:40 -0400

    install showplots correcly

--- install_thirdparty.sh
+++ install_thirdparty.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+mkdir output
+mkdir output/log
+cd output/log
+wget https://raw.githubusercontent.com/tswedish/trainplot/jeepers-logging/showplots.html
+cd ../..
 mkdir util_thirdparty
 cd util_thirdparty
 wget https://raw.githubusercontent.com/tswedish/fb.resnet.torch/jeepers-transforms/datasets/transforms.lua
